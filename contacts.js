3b75b2f3ae246269527314964c331badab6d332e
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2023-02-21 19:59:13 +0200

    completed

--- contacts.js
+++ contacts.js
@@ -1,4 +1,41 @@
 const fs = require('fs').promises;
 const path = require('path');
+const { v4 } = require('uuid');
 
-// const contactsPath
+const contactsPath = path.resolve(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+}
+
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const result = contacts.find(item => item.id === contactId);
+    return result;
+}
+
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const removedContact = contacts.find(item => item.id === contactId); 
+    const NewContactList = contacts.filter(item => item.id ==! contactId); 
+    fs.writeFile(contactsPath, JSON.stringify(NewContactList));
+    return removedContact;
+}
+
+async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    // const data = { name, email, phone };
+    const newContacts = { name, email, phone, id: v4() };
+    contacts.push(newContacts);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newContacts;
+}
+
+module.exports = {
+listContacts,
+getContactById,
+removeContact,
+addContact
+};
